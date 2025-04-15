7826f8ff0520d76b75c5ed2b58d1f8d36d0275c0
hxh <3148182735@qq.com>
2025-04-15 19:55:10 +0800

    实现字符表的存储和载入，以及根据词汇表解码token为字符串

--- data_process.py
+++ data_process.py
@@ -14,8 +14,7 @@ data = read_json(path)
 
 # 创建分词器
 class Tokenizer:
-    def __init__(self, vocab_size , sentences: list):
-        self.vocab_size = vocab_size
+    def __init__(self):
         self.vocabulary = {i : chr(i) for i in range(128)}
 
     # 获取相邻字符对 和 出现次数
@@ -25,6 +24,8 @@ class Tokenizer:
             pairs[pair] = pairs.get(pair, 0) + 1
 
         return pairs
+
+    # 合并字符对
     def __merge(self , code_list , pair , index) -> list:
         code_len = len(code_list)
         if code_len == 1:
@@ -42,14 +43,13 @@ class Tokenizer:
         return new_code_list
 
 
-
-    #TODO BPE构建词汇表
-    def build_vocabulary(self, sentences : list):
+    #BPE构建词汇表
+    def build_vocabulary(self, vocab_size , sentences : list):
         # 获取句子初始的字符编码列表
         raw_code = [ord(char) for sentence in sentences for char in sentence]
 
         index = len(self.vocabulary)
-        merge_num = self.vocab_size - index
+        merge_num = vocab_size - index
 
         # BPE
         for i in range(merge_num):
@@ -61,19 +61,41 @@ class Tokenizer:
             # 合并
             raw_code = self.__merge(raw_code, max_pair, index + i)
 
+    # 保存词汇表
+    def save_vocabulary(self, path = 'data.json'):
+        # 将字典保存为JSON文件
+        with open(path, 'w', encoding='utf-8') as json_file:
+            json.dump(self.vocabulary, json_file, ensure_ascii=False, indent=4)
+
+    # 加载词汇表
+    def load_vocabulary(self, path = 'data.json'):
+        # 从JSON文件加载字典
+        with open(path, 'r', encoding='utf-8') as json_file:
+            self.vocabulary = json.load(json_file)
+        self.vocabulary = {int(k): v for k, v in self.vocabulary.items()}
+        print(self.vocabulary)
 
 
-
-    def encode(self, sentence):
+    # TODO 编码
+    def encode(self, sentence: str):
         pass
-    def decode(self, tokens):
-        pass
+
+
+    # 解码
+    def decode(self, tokens: list) -> str:
+        for i in range(len(tokens)):
+            tokens[i] = self.vocabulary[tokens[i]]
+        return ''.join(tokens)
 
 
 
 
-tokenizer = Tokenizer(vocab_size=150, sentences=data['english'].head(10).to_list)
+tokenizer = Tokenizer()
 
-tokenizer.build_vocabulary(data['english'].head(2).to_list())
+#tokenizer.build_vocabulary(data['english'].head(2).to_list())
+#tokenizer.save_vocabulary()
+tokenizer.load_vocabulary()
+print(tokenizer.decode([148,145,136,112,65]))
+
 
 
