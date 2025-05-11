c19d5b4e214e3db1fa2cae24fd475d32e4396ae9
hxh <3148182735@qq.com>
2025-05-11 09:27:01 +0800

    实现多头注意力

--- Attention.py
+++ Attention.py
@@ -1,51 +1,89 @@
 import torch
 import torch.nn as nn
-
+# 多头注意力
 class Attention(nn.Module):
-    def __init__(self , embedding_size , d_K , d_V):
+    def __init__(self , d_model , d_K , d_V , num_heads):
          super(Attention , self).__init__()
          self.d_K = d_K
+         self.num_heads = num_heads
          # Q K V 权重矩阵
-         self.WQ = nn.Parameter(torch.randn(embedding_size, d_K))
-         self.WK = nn.Parameter(torch.randn(embedding_size, d_K))
-         self.WV = nn.Parameter(torch.randn(embedding_size, d_V))
+         self.WQ = nn.Linear(d_model, d_K * num_heads)
+         self.WK = nn.Linear(d_model, d_K * num_heads)
+         self.WV = nn.Linear(d_model, d_V * num_heads)
+         # 输出权重矩阵
+         self.W_O = nn.Linear(d_V * num_heads, d_model)
+
+    def single_attention(self , Q , K , V , mask = None):
+        # (B , T , d_K) @ (B , d_K , T) -> (B , T , T)
+        attention_score = Q @ K.transpose(-1, -2)
+        print("注意力得分",attention_score.shape)
+        attention_score /= torch.sqrt(torch.tensor(self.d_K))
+        # 掩码
+        if mask is not None:
+            attention_score = attention_score.masked_fill(mask, -float('inf'))
+        # 转为权重
+        attention_score = torch.softmax(attention_score, dim= -1)
+
+        # (B , T , T) @ (B , T , d_V) = (B , T , d_V)
+        return attention_score @ V
+
+    # 分割多头注意力
+    def split_heads(self , input , num_heads):
+        b , t , d_model = input.size()
+        # (B , T , d_ * num_heads) -> (B , T , num_heads , d_)
+        w = input.view(b, t, num_heads, -1)
+
+        # (B , T , num_heads , d_) -> (num_heads , B , T , d_)
+        # 深拷贝 w
+        w = w.permute(2, 0, 1, 3).contiguous()
 
 
+        return w
+    def combine_heads(self , input):
+        n , b , t , d_V = input.size()
+
+        # (num_heads , B , T , d_V) -> (B , T , num_heads , d_V)
+        w = input.permute(1, 2, 0, 3).contiguous()
+        print("注意力头移到后面",w.shape)
+        # (B , T , num_heads , d_V) -> (B , T , d_V * num_heads)
+        w = w.view(b, t, -1)
+        print("合并注意力头",w.shape)
+
+        return w
+
+
+    # (B , T , d_model)
     def forward(self , input_Q , input_KV = None , mask = None):
         if input_KV is None:
             input_KV = input_Q
 
         # 计算 Q K V
-        # (B , T , d_K)
-        Q = input_Q @ self.WQ
-        K = input_KV @ self.WK
-        # (B , T , d_V)
-        V = input_KV @ self.WV
+        # (B , T , d_K * num_heads)
+        Q = self.WQ(input_Q)
+        K = self.WK(input_KV)
+        # (B , T , d_V * num_heads)
+        V = self.WV(input_KV)
 
+        # 拆分注意力 转为单头注意力并行计算
+        # (B , T , d_ * num_heads) -> (num_heads , B , T , d_)
+        Q = self.split_heads(Q, self.num_heads)
+        K = self.split_heads(K, self.num_heads)
+        V = self.split_heads(V, self.num_heads)
 
+        # 单头注意力
+        # (num_heads , B , T , d_V)
+        attention_score = self.single_attention(Q, K, V, mask)
+        print("注意力结果",attention_score.shape)
 
-        # 计算注意力分数
-        #  (B , T , T)
-        attention_score = Q @ K.transpose(-1, -2)
+        # 合并多头注意力
+        # (num_heads , B , T , d_V) -> (B , T , d_V * num_heads)
+        combine = self.combine_heads(attention_score)
+        print("合并的结果",combine.shape)
+        #  (B , T , d_V * num_heads) -> (B , T , d_model)
+        output = self.W_O(combine)
+        print("转为d_model大小",output.shape)
 
+        return output
 
-        # 归一化
-        attention_score /= torch.sqrt(torch.tensor(self.d_K))
-
-        # 掩码
-        if mask is not None:
-            attention_score = attention_score.masked_fill(mask, -float('inf'))
-        print(attention_score)
-        # 转为权重
-        attention_score = torch.softmax(attention_score, dim= -1)
-
-        print(attention_score)
-        # 获取融入注意力的 V 矩阵
-        # (B , T , T) @ (B , T , d_V) = (B , T , d_V)
-        res = attention_score @ V
-
-
-
-        return V , res
 
 
